5365a30e52ad9428821279ef7bca83071b7e8408
koka <koka.code@gmail.com>
2017-06-20 09:49:08 +0900

    init

---
+++ lib/tlpi_hdr.h
@@ -0,0 +1,20 @@
+#ifndef TLPI_HDR_H_
+#define TLPI_HDR_H_
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "get_num.h"
+
+#include "error_functions.h"
+
+typedef enum {FALSE, TRUE} Boolean;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+#define max(m,n) ((m) > (n) ? (m) : (n))
+
+#endif // TLPI_HDR_H_
