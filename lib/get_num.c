6f35bf2bfc7f15284ce333eeb60b89d03e0e5da0
koka <koka.code@gmail.com>
2017-06-20 10:19:15 +0900

    impl getnum

--- lib/get_num.c
+++ lib/get_num.c
@@ -9,13 +9,43 @@
 static void
 gnFail(const char *fname, const char *msg, const char *arg, const char *name)
 {
+    fprintf(stderr, "%s err", fname);
+    if (name != NULL)
+        fprintf(stderr, "(in %s)", name);
+    fprintf(stderr, ": %s\n", msg);
+    if (arg != NULL && *arg != '\0')
+        fprintf(stderr, "gnfail %s\n", arg);
 
+    exit(EXIT_FAILURE);
 }
 
 static long
 getNum(const char *fname, const char *arg, int flags, const char *name)
 {
     long res;
+    char *endptr;
+    int base;
+
+    if (arg == NULL || *arg == '\0')
+        gnFail(fname, "null or empty string", arg, name);
+
+    base = (flags & GN_ANY_BASE) ? 0 : (flags & GN_BASE_8) ? 8 :
+                (flags & GN_BASE_16) ? 16 : 10;
+
+    errno = 0;
+    res = strtol(arg, &endptr, base);
+    if (errno != 0)
+        gnFail(fname, "strtol", arg, name);
+
+    if (*endptr != '\0')
+        gnFail(fname, "non-numeric", arg, name);
+
+    if ((flags & GN_NONNEG) && res <= 0)
+        gnFail(fname, "negative", arg, name);
+
+    if ((flags & GN_GT_0) && res <= 0)
+        gnFail(fname, ">0", arg, name);
+
     return res;
 }
 
